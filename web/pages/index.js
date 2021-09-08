bbbd99665d683d9e36cbd239701cb4525af22bad
Adam LeCorney <adamlecorney@gmail.com>
2021-09-09 01:43:33 +0200

    add arrow buttons that reacts on key press

--- web/pages/index.js
+++ web/pages/index.js
@@ -3,6 +3,7 @@ import { useState, useEffect, useContext } from 'react'
 import styled from 'styled-components'
 import { Map } from '../components/Map'
 import { SocketContextProvider, SocketContext } from '../components/socket'
+import { useKeyPress } from '../components/useKeyPress'
 
 const Container = styled.div`
   min-height: 100vh;
@@ -11,6 +12,42 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
 `
+
+const AAlenControl = styled.div`
+  margin-bottom: 20px;
+`
+
+const Buttons = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const ButtonCol = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const CommandButton = styled.button`
+  padding: 10px 16px;
+
+  ${({ keyPressed }) => keyPressed && 'background-color: lightgrey;'}
+`
+
+const KeyButton = ({ targetKey, label, onPress }) => {
+  const keyPressed = useKeyPress(targetKey)
+
+  useEffect(() => {
+    if (keyPressed) {
+      onPress()
+    }
+  }, [keyPressed])
+
+  return (
+    <CommandButton onClick={onPress} keyPressed={keyPressed}>
+      {label}
+    </CommandButton>
+  )
+}
 
 const Home = () => {
   const { socket } = useContext(SocketContext)
@@ -36,22 +73,15 @@ const Home = () => {
   return (
     <Container>
       <Head>
-        <title>Ålen data</title>
+        <title>Ålen</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1>Ålen data</h1>
+        <h1>Ålen</h1>
         <div>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              value={currentCommand}
-              onChange={(e) => {
-                setCurrentCommand(e.target.value)
-              }}
-            ></input>
-          </form>
+          <h2>Data control</h2>
+
           <button
             onClick={() => {
               fetch('/start')
@@ -76,6 +106,30 @@ const Home = () => {
             Rensa
           </button>
         </div>
+        <AAlenControl>
+          <h2>Ålen control</h2>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              value={currentCommand}
+              onChange={(e) => {
+                setCurrentCommand(e.target.value)
+              }}
+            ></input>
+          </form>
+          <Buttons>
+            <ButtonCol>
+              <KeyButton label="<" targetKey="ArrowLeft" onPress={() => {}} />
+            </ButtonCol>
+            <ButtonCol>
+              <KeyButton label="^" targetKey="ArrowUp" onPress={() => {}} />
+              <KeyButton label="v" targetKey="ArrowDown" onPress={() => {}} />
+            </ButtonCol>
+            <ButtonCol>
+              <KeyButton label=">" targetKey="ArrowRight" onPress={() => {}} />
+            </ButtonCol>
+          </Buttons>
+        </AAlenControl>
         <Map
           markerPosition={positions.length > 0 ? positions[positions.length - 1] : null}
           polylinePositions={positions}
