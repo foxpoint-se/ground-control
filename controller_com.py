f16b508a7d93d30ca4815feb3e88de46355e9465
bryndav <d.bryngelsson@gmail.com>
2021-11-14 22:40:01 +0100

    Draft for sending val

--- controller_com.py
+++ controller_com.py
@@ -1,13 +1,27 @@
+import serial
+
 from gp import GP, ButtonCodes, translate_from_range_to_range
+
+
+class HC12:
+    def __init__(self):
+        self.ser = serial.Serial("/dev/USB0", 9600)
+
+    def send(self, msg):
+        self.ser.write(bytes(msg, encoding='utf-8'))
+
+
+com = HC12()
 
 
 def my_event_handler(event):
     if event.button_code == ButtonCodes.LEFT_Y:
         translated_value = translate_from_range_to_range(event.value, -1, 1, -255, 255)
-        print(f"Value to be sent: {translated_value}")
     elif event.button_code == ButtonCodes.RIGHT_X:
         translated_value = translate_from_range_to_range(event.value, -1, 1, 750, 2250)
-        print(f"Value to be sent: {translated_value}")
+
+    print(f"Sending value: {translated_value}")
+    com.send(translated_value)
 
 
 if __name__ == '__main__':
