363df242d5d115b79afb3d3bcc9cd100407346bc
bryndav <d.bryngelsson@gmail.com>
2021-11-14 22:08:27 +0100

    Draft controller

---
+++ controller_com.py
@@ -0,0 +1,15 @@
+from gp import GP, ButtonCodes, translate_from_range_to_range
+
+
+def my_event_handler(event):
+    if event.button_code == ButtonCodes.LEFT_Y:
+        translated_value = translate_from_range_to_range(event.value, -1, 1, -255, 255)
+        print(f"Value to be sent: {translated_value}")
+    elif event.button_code == ButtonCodes.RIGHT_Y:
+        translated_value = translate_from_range_to_range(event.value, -1, 1, 750, 2250)
+        print(f"Value to be sent: {translated_value}")
+
+
+if __name__ == '__main__':
+    gp = GP(event_handler=my_event_handler)
+    input("Press enter to exit\n")
