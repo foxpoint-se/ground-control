fe019d61c41ed8b2d0d3c1d6e9c712f6c69dc0ac
bryndav <d.bryngelsson@gmail.com>
2021-11-14 22:23:45 +0100

    Show raw output

--- controller_com.py
+++ controller_com.py
@@ -5,11 +5,9 @@ def my_event_handler(event):
     if event.button_code == ButtonCodes.LEFT_Y:
         translated_value = translate_from_range_to_range(event.value, -1, 1, -255, 255)
         print(f"Value to be sent: {translated_value}")
-    # elif event.button_code == ButtonCodes.RIGHT_Y:
-    #     translated_value = event.value
-    #     print(f"RIGHT Y VALUE: {translated_value}")
     elif event.button_code == ButtonCodes.RIGHT_X:
-        print(f"RIGHT X VALUE: {event.value}")
+        translated_value = translate_from_range_to_range(event.value, -1, 1, 750, 2250)
+        print(f"Value to be sent: {translated_value}")
 
 
 if __name__ == '__main__':
