da17ab0d706254c3d36e17302127e1e2dcfa7eab
bulingen <adamlecorney@gmail.com>
2022-01-08 10:25:59 +0100

    feat: use simulation port in (useless?) script

--- read.py
+++ read.py
@@ -4,7 +4,9 @@ import time
 
 baudrate = 9600
 # port_name = 'COM5'
-port_name = '/dev/ttyUSB0'
+# port_name = '/dev/ttyUSB0'
+# port_name = '/dev/pts/11'
+port_name = '/tmp/virtual_serial_connect'
 # port_name = '/dev/ttyS0'
 
 print('starting to read baudrate', baudrate, 'port name', port_name)
@@ -19,8 +21,9 @@ while True:
     # decode("utf-8") ???
     line = ser.readline()   # read a byte
     if line:
-        string = line.decode()  # convert the byte string to a unicode string
+        string = line.decode().strip()  # convert the byte string to a unicode string
         print(string)
+        time.sleep(0.01)
         # num = int(string)  # convert the unicode string to an int
         # print(num)
 
