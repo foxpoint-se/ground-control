b013b7978af5f6b7429776cc7bab264378e45aea
bulingen <adamlecorney@gmail.com>
2022-01-12 00:01:51 +0100

    chore: remove unused

--- utils/serial_helpers.py
+++ utils/serial_helpers.py
@@ -1,60 +1,8 @@
 import atexit
-import traceback
 import sys
 import serial
-import threading
-import time
-from typing import Any, Callable
+from typing import Callable
 from serial.threaded import LineReader, ReaderThread
-
-
-# class SerialReader(threading.Thread):
-#     def __init__(self, on_message: Callable[[], str], serial: str):
-#         super(SerialReader, self).__init__()
-#         self.daemon = True
-#         self.ser = serial
-#         self.on_message = on_message
-#         self.should_run = True
-#         self.start()
-
-#     def run(self):
-#         self.readlines()
-
-#     def pause(self):
-#         self.should_run = False
-
-#     def resume(self):
-#         self.should_run = True
-
-#     def readlines(self):
-#         while True:
-#             if self.should_run and self.on_message:
-#                 try:
-#                     pass
-#                 except Exception as err:
-#                     pass
-#                 line = self.ser.readline()
-#                 if line:
-#                     message = line.decode("utf-8").strip()
-#                     self.on_message(message)
-#                 # time.sleep(0.01)
-
-
-# class SerialReaderWriter2:
-#     def __init__(
-#         self,
-#         port: str,
-#         baudrate: int = 9600,
-#         timeout: int = None,
-#         on_message: Callable[[], str] = None,
-#     ) -> None:
-#         self.serial = serial.Serial(port=port, baudrate=baudrate, timeout=timeout)
-#         if on_message:
-#             self.reader = SerialReader(on_message, self.serial)
-
-#     def send(self, message) -> str:
-#         if message:
-#             self.serial.write(bytes("{}\n".format(message), encoding="utf-8"))
 
 
 class ReaderWrapper(LineReader):
