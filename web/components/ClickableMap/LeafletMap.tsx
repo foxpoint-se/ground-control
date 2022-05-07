86b691ce350eb656a5e1a1fb559927e19f872c2e
bulingen <adamlecorney@gmail.com>
2022-05-07 13:06:41 +0200

    wip: typescript map for ros page

---
+++ web/components/ClickableMap/LeafletMap.tsx
@@ -0,0 +1,165 @@
+import { Circle, MapContainer, Marker, Polyline, TileLayer, useMapEvents } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+import { Fragment } from 'react'
+import icon from 'leaflet/dist/images/marker-icon.png'
+import { RotatableMarker } from './RotatableMarker'
+
+export interface Coordinate {
+  lat: number
+  lon: number
+}
+
+interface PolylineOpts {
+  id: string
+  color: string
+  coordinates: Coordinate[]
+}
+
+export type IconType = 'dot' | 'arrowline' | 'pin'
+
+export interface MarkerOpts extends Coordinate {
+  icon: IconType
+  rotated?: boolean
+  rotationAngle?: number
+  zIndexOffset?: number
+}
+
+export interface TargetMarkerOpts extends MarkerOpts {
+  tolerance: number
+}
+
+interface LeafletMapProps {
+  width: number | string
+  height: number | string
+  polylines?: PolylineOpts[]
+  heading?: number
+  position?: [number, number]
+  markers?: MarkerOpts[]
+  targetMarkers?: TargetMarkerOpts[]
+  arrowLineMarker?: MarkerOpts
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
+}
+
+const ArrowLineIcon = L.icon({
+  iconUrl: '/arrowline.svg',
+  iconSize: [200, 200],
+  iconAnchor: [100, 184],
+})
+
+const DotIcon = L.icon({
+  iconUrl: '/dot.svg',
+  iconSize: [12, 12],
+  iconAnchor: [6, 6],
+})
+
+const PinIcon = L.icon({
+  iconUrl: icon.src,
+  iconSize: [28, 44],
+  iconAnchor: [13, 41],
+})
+
+const OnClickHandler = ({ onClick }) => {
+  useMapEvents({
+    click: onClick,
+  })
+  return null
+}
+
+type Icons = Record<IconType, L.Icon>
+const icons: Icons = {
+  dot: DotIcon,
+  arrowline: ArrowLineIcon,
+  pin: PinIcon,
+}
+
+const LeafletMap = ({
+  width,
+  height,
+  polylines = [],
+  markers = [],
+  targetMarkers = [],
+  arrowLineMarker,
+  onClick,
+}: LeafletMapProps) => {
+  // const [angle, setAngle] = useState(0)
+  // const [currentPositionIndex, setCurrentPositionIndex] = useState(0)
+  // const [currentPosition, setCurrentPosition] = useState(routes[2].path[0])
+
+  // let interval
+  // useEffect(() => {
+  //   setInterval(() => {
+  //     setAngle((prev) => (prev + 1) % 360)
+  //     setCurrentPositionIndex((prev) => (prev + 1) % routes[2].path.length)
+  //   }, 10)
+
+  //   return () => {
+  //     clearInterval(interval)
+  //   }
+  // }, [])
+
+  return (
+    <MapContainer
+      center={[59.310506, 17.981233]}
+      zoom={16}
+      scrollWheelZoom={true}
+      style={{ height, width }}
+    >
+      {onClick && <OnClickHandler onClick={onClick} />}
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {targetMarkers.map(({ icon, lat, lon, tolerance }) => {
+        return (
+          <Fragment key={`${lat}${lon}`}>
+            <Circle
+              center={[lat, lon]}
+              radius={2 * tolerance}
+              stroke={false}
+              fill
+              fillOpacity={0.3}
+              fillColor="#419c43"
+            />
+            <Circle
+              center={[lat, lon]}
+              radius={tolerance}
+              weight={10}
+              stroke={false}
+              fillColor="#419c43"
+              fillOpacity={0.5}
+            />
+            <Marker icon={icons[icon]} position={[lat, lon]} />
+          </Fragment>
+        )
+      })}
+      {polylines.map(({ id, color, coordinates }) => (
+        <Polyline
+          key={id}
+          pathOptions={{ color }}
+          positions={coordinates
+            .filter(({ lat, lon }) => lat && lon)
+            .map(({ lat, lon }) => [lat, lon])}
+        />
+      ))}
+      {markers.map(({ icon, lat, lon, zIndexOffset }) => {
+        return (
+          <Marker
+            key={`${lat}${lon}`}
+            icon={icons[icon]}
+            position={[lat, lon]}
+            zIndexOffset={zIndexOffset}
+          />
+        )
+      })}
+      <RotatableMarker
+        position={[arrowLineMarker.lat, arrowLineMarker.lon]}
+        icon={icons[arrowLineMarker.icon]}
+        rotationAngle={arrowLineMarker.rotationAngle}
+        zIndexOffset={arrowLineMarker.zIndexOffset}
+      />
+    </MapContainer>
+  )
+}
+
+export default LeafletMap
--- web/pages/ros.js
+++ web/pages/ros.js
@@ -2,10 +2,13 @@ import { useEffect, useState } from 'react'
 import ROSLIB from 'roslib'
 import { DataSheet } from '../components/DataSheet'
 import { Compass } from '../components/Compass'
+import { ClickableMap } from '../components/ClickableMap'
+import { Container, Main } from '../components/styles'
 
 const RosPage = () => {
   const [imuStatus, setImuStatus] = useState({})
   const [gnssStatus, setGnssStatus] = useState({})
+  const [navStatus, setNavStatus] = useState()
 
   useEffect(() => {
     var ros = new ROSLIB.Ros({
@@ -36,6 +39,12 @@ const RosPage = () => {
       messageType: 'eel_interfaces/GnssStatus',
     })
 
+    const navListener = new ROSLIB.Topic({
+      ros,
+      name: '/nav/status',
+      messageType: 'eel_interfaces/NavigationStatus',
+    })
+
     listener.subscribe(function (message) {
       // console.log('Received message on ', listener.name, message)
       setImuStatus(message)
@@ -44,23 +53,76 @@ const RosPage = () => {
     gnssListener.subscribe(function (message) {
       setGnssStatus(message)
     })
+    navListener.subscribe(function (message) {
+      // if (!navStatus) {
+      setNavStatus(message)
+      // }
+    })
   }, [])
 
+  // console.log({ imuStatus, gnssStatus })
+
+  // TODO: types
+  //   auto_mode_enabled: false
+  // meters_to_target: 101.11756896972656
+  // next_target: [{…}]
+  // tolerance_in_meters: 5
+
+  //   gnssStatus: Message
+  // lat: 59.31022644042969
+  // lon: 17.97468376159668
+
+  // imuStatus: Message
+  // accel: 3
+  // euler_heading: 353.8733215332031
+  // gyro: 3
+  // is_calibrated: true
+  // mag: 3
+  // sys: 3
+
+  const targetMarkers = []
+  if (navStatus) {
+    targetMarkers.push({
+      tolerance: navStatus.tolerance_in_meters,
+    })
+  }
+
   return (
-    <div>
-      <div>
-        <h1>Här är internt ROS-state</h1>
-        <div>Heading: {imuStatus.euler_heading}</div>
-        <div>Lat: {gnssStatus.lat}</div>
-        <div>Lon: {gnssStatus.lon}</div>
-      </div>
-      <div>
-        <DataSheet />
-      </div>
-      <div>
-        <Compass heading={imuStatus?.euler_heading} />
-      </div>
-    </div>
+    <Container>
+      <Main>
+        <div>
+          <h1>Här är internt ROS-state</h1>
+          <div>Heading: {imuStatus.euler_heading}</div>
+          <div>Lat: {gnssStatus.lat}</div>
+          <div>Lon: {gnssStatus.lon}</div>
+        </div>
+        <div>
+          <DataSheet />
+        </div>
+        <div>
+          <Compass heading={imuStatus?.euler_heading} />
+        </div>
+        <ClickableMap
+          vehicle={
+            gnssStatus?.lat &&
+            gnssStatus?.lon && {
+              coordinate: { lat: gnssStatus.lat, lon: gnssStatus.lon },
+              heading: imuStatus?.euler_heading || 0,
+            }
+          }
+          targetMarkers={
+            navStatus?.next_target[0] && [
+              {
+                tolerance: navStatus.tolerance_in_meters,
+                icon: 'pin',
+                lat: navStatus.next_target[0].lat,
+                lon: navStatus.next_target[0].lon,
+              },
+            ]
+          }
+        />
+      </Main>
+    </Container>
   )
 }
 
