1a0e11a93f7d4c02b37214529ea75d7e7fbc2441
bulingen <adamlecorney@gmail.com>
2024-01-31 01:07:06 +0100

    fix: slightly better things index page

--- web/src/app/iot/things/page.tsx
+++ web/src/app/iot/things/page.tsx
@@ -1,7 +1,15 @@
 "use client";
 
 const Page = () => {
-  return <h1>Things</h1>;
+  return (
+    <main className="px-md max-w-3xl mx-auto">
+      <h1 className="text-5xl font-bold mb-md">My things</h1>
+      <p>
+        This page is mostly here for completeness. We can put all kinds of
+        operations here.
+      </p>
+    </main>
+  );
 };
 
 export default Page;
