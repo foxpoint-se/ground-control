a28eed92f9b2f9d0d5fdcad76aea7383e5ee1def
bulingen <adamlecorney@gmail.com>
2024-08-30 19:54:53 +0200

    feat(iot): subscribe to route tracing and draw

--- web/src/app/iot/things/components/IotMap.tsx
+++ web/src/app/iot/things/components/IotMap.tsx
@@ -1,10 +1,11 @@
 import { Coordinate } from "@/app/components/mapTypes";
-import { ImuStatus } from "@/app/components/topics";
+import { ImuStatus, TracedRoute } from "@/app/components/topics";
 import { useState } from "react";
 import {
   useGnssSubscriber,
   useImuSubscriber,
   useLocalizationSubscriber,
+  useTracedRouteSubscriber,
 } from "./useSubscribeToTopic";
 import { MapPanel } from "@/app/components/map/MapPanel";
 
@@ -12,9 +13,17 @@ export const IotMap = ({ thingName }: { thingName: string }) => {
   const [vehiclePosition, setVehiclePosition] = useState<Coordinate>();
   const [ghostPosition, setGhostPosition] = useState<Coordinate>();
   const [imuStatus, setImuStatus] = useState<ImuStatus>();
+  const [tracedRoutes, setTracedRoutes] = useState<TracedRoute[]>([]);
   useImuSubscriber(thingName, setImuStatus);
   useGnssSubscriber(thingName, setVehiclePosition);
   useLocalizationSubscriber(thingName, setGhostPosition);
+
+  useTracedRouteSubscriber(thingName, (newSegment: TracedRoute) => {
+    setTracedRoutes((prev) => {
+      const newList = [...prev, newSegment];
+      return newList;
+    });
+  });
 
   const onUpdateGnss = (c: Coordinate) => {
     console.log("Not implemented. Coord:", c);
@@ -26,6 +35,7 @@ export const IotMap = ({ thingName }: { thingName: string }) => {
       vehicleRotation={imuStatus?.heading}
       onUpdateGnss={onUpdateGnss}
       ghostPosition={ghostPosition}
+      tracedRoutes={tracedRoutes}
     />
   );
 };
