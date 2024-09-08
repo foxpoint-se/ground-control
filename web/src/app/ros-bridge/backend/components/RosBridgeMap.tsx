5ba01927c42cb9a833a429a8ecb600a605b505dd
bulingen <adamlecorney@gmail.com>
2024-09-08 11:01:01 +0200

    feat: mission creation with settings for depth and sync yes/no

--- web/src/app/ros-bridge/backend/components/RosBridgeMap.tsx
+++ web/src/app/ros-bridge/backend/components/RosBridgeMap.tsx
@@ -18,6 +18,7 @@ import {
 import { Panel } from "@/app/components/Panel";
 import { Toggle } from "@/app/components/Toggle";
 import { DistanceDebug, Entry } from "./DistanceDebug";
+import { calcCrowDistanceMeters } from "@/app/components/calcDistance";
 
 export const RosBridgeMap = ({ rosBridge }: { rosBridge: ROSLIB.Ros }) => {
   const [isDistanceDebugEnabled, setIsDistanceDebugEnabled] = useState(false);
@@ -154,28 +155,6 @@ const createPopupText = (entries: Entry[]): ReactNode => {
     );
   }
   return "Select exactly 2 positions";
-};
-
-const calcCrowDistanceMeters = (
-  coordinate1: Coordinate,
-  coordinate2: Coordinate
-): number => {
-  const R = 6371000; // meters
-  const dLat = toRadians(coordinate2.lat - coordinate1.lat);
-  var dLon = toRadians(coordinate2.lon - coordinate1.lon);
-  var lat1 = toRadians(coordinate1.lat);
-  var lat2 = toRadians(coordinate2.lat);
-
-  var a =
-    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  var d = R * c;
-  return d;
-};
-
-const toRadians = (degrees: number) => {
-  return (degrees * Math.PI) / 180;
 };
 
 const DistanceDisplay = ({ clickedEntries }: { clickedEntries: Entry[] }) => {
